b02823603f07b67cbf3dcab22ef76e1abf373b44
marcelo <malvarengajr@gmail.com>
2021-11-15 20:30:09 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <NavBar/>
+     
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import NavBar from './components/NavBar.vue';
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    NavBar
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<v-toolbar color="primary">
+			<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+		</v-toolbar>
+	<div>
+
+	</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
